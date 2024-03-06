a716f66b1dd415b947291520c0b54b6e83ad8e92
unknown <mdamir4298@gmail.com>
2024-03-07 02:43:01 +0530

    navbar,intro, and idoevent created

--- frontend/my-app/src/App.js
+++ frontend/my-app/src/App.js
@@ -1,13 +1,14 @@
 import "./App.css";
+import IdoEvent from "./Components/IdoEvent";
+import Intro from "./Components/Intro";
 import Navbar from "./Components/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <div className="bg-red-500">tailwind css</div>
-      
-      <h1 className="text-3xl font-bold underline">hello react</h1>
+      <Navbar /> 
+      <Intro />
+      <IdoEvent/>
     </div>
   );
 }
--- frontend/my-app/src/Components/Navbar.jsx
+++ frontend/my-app/src/Components/Navbar.jsx
@@ -1,26 +1,38 @@
 import React from "react";
+import saviourLogo from "../assets/LogoSavior-red.png";
+import "../styles/navbar.css";
+import bg from "../assets/Rectangle18.png"
 
 const Navbar = () => {
   return (
-    <div>
+    <div className="container">
+    
       {/* Navbar start here */}
-      <div>
-        <div>
-          <img src="" alt="" />
-          <p>Saviour</p>
+      <div className="navbar  flex justify-around items-center ">
+        <div className="nav_child1 flex items-center text-center">
+          <img className="logo " src={saviourLogo} alt="saviourLogo" />
+          <p className=" ">Saviour</p>
         </div>
-        <div>
-          <div>
+        <div className="nav_child2 flex justify-between items-center">
+          <div className="nav_links flex items-center px-4 py-6 gap-6 ">
             <a href="/">Home</a>
             <a href="/">IDO</a>
             <a href="/">Tokenomics</a>
             <a href="/">Roadmap</a>
             <a href="/">Coming Soon</a>
           </div>
-          <button>Connect Wallet</button>
+          
         </div>
+        <div className="connect_wallet">
+            {" "}
+            <button >Connect Wallet</button>
+          </div>
       </div>
 
+      {/* main part */}
+      <div className="main_content">
+        <p>Where Blockchain Heroes Thrive, Rescuing Dreams, Elevating Fortunes.</p>
+      </div>
     </div>
   );
 };
