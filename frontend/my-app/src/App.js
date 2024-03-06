682703b4b64096873330f66c226c74bed80f2ee1
unknown <mdamir4298@gmail.com>
2024-03-06 17:37:25 +0530

    initial setup for frontend done

---
+++ frontend/my-app/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import Navbar from "./Components/Navbar";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <div className="bg-red-500">tailwind css</div>
+      
+      <h1 className="text-3xl font-bold underline">hello react</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/my-app/src/Components/Navbar.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+      {/* Navbar start here */}
+      <div>
+        <div>
+          <img src="" alt="" />
+          <p>Saviour</p>
+        </div>
+        <div>
+          <div>
+            <a href="/">Home</a>
+            <a href="/">IDO</a>
+            <a href="/">Tokenomics</a>
+            <a href="/">Roadmap</a>
+            <a href="/">Coming Soon</a>
+          </div>
+          <button>Connect Wallet</button>
+        </div>
+      </div>
+
+    </div>
+  );
+};
+
+export default Navbar;
